45819d9263a3651c91e72c63fc15a11f05bb28d2
idaxyt <waitingida@live.com>
2019-05-02 19:09:36 +0800

    引入推荐组件

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,21 +1,24 @@
 <template>
-    <div>
-        <home-header></home-header>
-        <home-swiper></home-swiper>
-        <home-icons></home-icons>
-    </div>
+  <div>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+    <home-recommend></home-recommend>
+  </div>
 </template>
 
 <script>
-import HomeHeader from './components/Header'
-import HomeSwiper from './components/Swiper'
-import HomeIcons from './components/Icons'
+import HomeHeader from "./components/Header";
+import HomeSwiper from "./components/Swiper";
+import HomeIcons from "./components/Icons";
+import HomeRecommend from "./components/Recommend";
 export default {
   name: "Home",
   components: {
-      HomeHeader,
-      HomeSwiper,
-      HomeIcons
+    HomeHeader,
+    HomeSwiper,
+    HomeIcons,
+    HomeRecommend
   }
 };
 </script>
