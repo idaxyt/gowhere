58518b4718501e6073dd1401ae9f7119c297eb52
idaxyt <waitingida@live.com>
2019-05-02 19:08:32 +0800

    添加margin-top

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -106,44 +106,48 @@ export default {
 .icons >>> .swiper-container {
   height: 0;
   padding-bottom: 50%;
-}
 
-.icon {
-  position: relative;
-  overflow: hidden;
-  float: left;
-  width: 25%;
-  height: 0;
-  padding-bottom: 25%;
-
-  .icon-img {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0.44rem;
-    box-sizing: border-box;
-    padding: 0.1rem;
-
-    .icon-img-content {
-      // 居中
-      display: block;
-      margin: 0 auto;
-      // 居中
-      height: 100%;
-    }
+  .icons {
+    margin-top: 0.1rem;
   }
 
-  .icon-desc {
-    position: absolute;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    height: 0.44rem;
-    line-heigh: 0.44rem;
-    color: $darkTextColor;
-    text-align: center;
-    ellipsis();
+  .icon {
+    position: relative;
+    overflow: hidden;
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-bottom: 25%;
+
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+      box-sizing: border-box;
+      padding: 0.1rem;
+
+      .icon-img-content {
+        // 居中
+        display: block;
+        margin: 0 auto;
+        // 居中
+        height: 100%;
+      }
+    }
+
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      height: 0.44rem;
+      line-heigh: 0.44rem;
+      color: $darkTextColor;
+      text-align: center;
+      ellipsis();
+    }
   }
 }
 </style>
