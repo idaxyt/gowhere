4702bc05c99c5f48ccebfa9d80414bb90afa5081
idaxyt <waitingida@live.com>
2019-05-02 11:46:07 +0800

    数据渲染用循环渲染得到

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,74 +1,149 @@
 <template>
-    <div class="icons">
-        <swiper :options='swiperOption'>
-            <swiper-slide>
-                <div class='icon'>
-                    <div class="icon-img">
-                        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
-                    </div>
-                    <p class="icon-desc">热门景点</p>
-                </div>
-            </swiper-slide>
-            <swiper-slide>
-                <div class='icon'>
-                    <div class="icon-img">
-                        <img class="icon-img-content" src="http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png" />
-                    </div>
-                    <p class="icon-desc">热门景点</p>
-                </div>
-            </swiper-slide>
-        </swiper>
-    </div>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page,index) in pages" :key="index">
+        <div class="icon" v-for="item in page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl">
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'HomeIcons',
-    data() {
-        return {
-            swiperOption: {
-                loop: true
-            }
+  name: "HomeIcons",
+  data() {
+    return {
+      swiperOption: {},
+      iconsList: [
+        {
+          id: "0001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          desc: "景点门票13123"
+        },
+        {
+          id: "0002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/97/02f5043b51b2102.png",
+          desc: "峨眉山"
+        },
+        {
+          id: "0003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+          desc: "绵阳必游"
+        },
+        {
+          id: "0004",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png",
+          desc: "赏花踏青"
+        },
+        {
+          id: "0005",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/ab/6f7d6e44963c9302.png",
+          desc: "温泉"
+        },
+        {
+          id: "0006",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/90/59a2f523ee2f9202.png",
+          desc: "农家度假"
+        },
+        {
+          id: "0007",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/97/02f5043b51b2102.png",
+          desc: "九黄山景区"
+        },
+        {
+          id: "0008",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png",
+          desc: "文化古迹"
+        },
+        {
+          id: "0009",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/48/cb72b99b71974c02.png",
+          desc: "生活休闲"
+        },
+        {
+          id: "0010",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png",
+          desc: "亲子游"
         }
+      ]
+    };
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconsList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
     }
-}
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
-    @import  '~styles/varibles.styl'
-    .icons
-        overflow: hidden
-        width: 100%
-        height: 0
-        padding-bottom: 50%
-    .icon
-        position: relative
-        overflow: hidden
-        float: left
-        width: 25%
-        height: 0
-        padding-bottom: 25%
-        .icon-img
-            position: absolute
-            top: 0
-            left: 0
-            right: 0
-            bottom: 0.44rem
-            box-sizing: border-box
-            padding: .1rem
-            .icon-img-content
-                //居中
-                display: block
-                margin: 0 auto
-                //居中
-                height: 100%
-        .icon-desc
-            position: absolute 
-            left: 0
-            right: 0
-            bottom: 0
-            height: 0.44rem
-            line-heigh: 0.44rem
-            color: $darkTextColor
-            text-align: center
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+.icons >>> .swiper-container {
+  height: 0;
+  padding-bottom: 50%;
+}
+
+.icon {
+  position: relative;
+  overflow: hidden;
+  float: left;
+  width: 25%;
+  height: 0;
+  padding-bottom: 25%;
+
+  .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0.44rem;
+    box-sizing: border-box;
+    padding: 0.1rem;
+
+    .icon-img-content {
+      // 居中
+      display: block;
+      margin: 0 auto;
+      // 居中
+      height: 100%;
+    }
+  }
+
+  .icon-desc {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    height: 0.44rem;
+    line-heigh: 0.44rem;
+    color: $darkTextColor;
+    text-align: center;
+    ellipsis();
+  }
+}
 </style>
