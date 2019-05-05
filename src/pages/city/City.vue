97012e0f801629ba9883cf1cb76dec6ba0bc55db
ida <ida@tansuyun.cn>
2019-05-05 18:20:55 +0800

    city数据通过ajax渲染

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,12 +2,13 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>  
-        <city-list></city-list>
-        <alphabet></alphabet>
+        <city-list  :cities='cities' :hot='hotCities'></city-list>
+        <alphabet :alpha='cities'></alphabet>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
@@ -19,7 +20,31 @@ export default {
         CitySearch,
         CityList,
         Alphabet
-    }
+    },
+    data() {
+        return {
+            cities: {
+
+            },
+            hotCities: []
+        }
+    },
+    methods:{
+        getCityInfo() {
+            axios.get('/api/city.json').then(this.handleCityInfoSucc)
+        },
+        handleCityInfoSucc(res) {
+            res = res.data;
+            if(res.ret && res.data) {
+                const data = res.data
+                this.cities = data.cities
+                this.hotCities = data.hotCities
+            }
+        }
+    },
+    mounted() {
+        this.getCityInfo()
+    },
 }
 </script>
 
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,17 +1,15 @@
 <template>
     <ul class="list">
-        <li class='item'>A</li>
-        <li class='item'>A</li>
-        <li class='item'>A</li>
-        <li class='item'>A</li>
-        <li class='item'>A</li>
-        <li class='item'>A</li>
+        <li class='item' v-for="(item,key) of alpha" :key="key">{{key}}</li>
     </ul>
 </template>
 
 <script>
 export default {
-    name: 'Alphabet'
+    name: 'Alphabet',
+    props: {
+        alpha: Object
+    }
 }
 </script>
 
