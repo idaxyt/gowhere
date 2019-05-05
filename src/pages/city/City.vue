cf904d91978991136ee39bed1a9a7255e939210e
ida <ida@tansuyun.cn>
2019-05-05 17:58:58 +0800

    滚动插件better-scroll+固定字母表

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>  
+        <city-list></city-list>
+        <alphabet></alphabet>
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import Alphabet from './components/Alphabet'
 export default {
     name: 'City',
     components: {
         CityHeader,
-        CitySearch
+        CitySearch,
+        CityList,
+        Alphabet
     }
 }
 </script>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+    <ul class="list">
+        <li class='item'>A</li>
+        <li class='item'>A</li>
+        <li class='item'>A</li>
+        <li class='item'>A</li>
+        <li class='item'>A</li>
+        <li class='item'>A</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'Alphabet'
+}
+</script>
+
+<style lang='stylus' scoped>
+@import '~styles/varibles.styl'
+.list
+    // 垂直方向上居中
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    // 垂直方向上居中    
+    position: absolute;
+    top: 1.58rem;
+    right:0;
+    bottom:0;
+    width: .4rem;
+    .item
+        line-height: .4rem;
+        color: $bgColor;
+        text-align: center;
+</style>
