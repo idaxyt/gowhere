8f51660993435c8ed50cd7362d463cdf5640ee20
idaxyt <waitingida@live.com>
2019-05-06 12:01:52 +0800

    兄弟组件联动之touch字母表联动城市列表

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,15 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,key) of alpha" :key="key" @click="handleClick">{{key}}</li>
+    <li
+      class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @click="handleClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @touchend="handleTouchEnd"
+    >{{item}}</li>
   </ul>
 </template>
 
@@ -10,9 +19,39 @@ export default {
   props: {
     alpha: Object
   },
+  computed: {
+    letters() {
+      const letters = [];
+      for (let i in this.alpha) {
+        letters.push(i);
+      }
+      return letters;
+    }
+  },
+  data() {
+    return {
+      touchStatus: false
+    };
+  },
   methods: {
     handleClick(e) {
       this.$emit("change", e.target.innerText);
+    },
+    handleTouchStart() {
+      this.touchStatus = true;
+    },
+    handleTouchMove(e) {
+      if (this.touchStatus) {
+        const startY = this.$refs["A"][0].offsetTop;
+        const touchY = e.touches[0].clientY - 81;
+        const index = Math.floor((touchY - startY) / 20);
+        if (index >= 0 && index < this.letters.length) {
+          this.$emit("change", this.letters[index]);
+        }
+      }
+    },
+    handleTouchEnd() {
+      this.touchStatus = false;
     }
   }
 };
