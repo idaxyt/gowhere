5e034e7dfd30af8867ee1b561f9a25846038ed8d
idaxyt <waitingida@live.com>
2019-05-06 18:55:25 +0800

    gallary组件布局+swiper插件多属性使用

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="warpper">
+      <swiper :options="swiperOption" v-for="(item,index) in imgs" :key="index">
+        <swiper-slide>
+          <img class="gallary-img" :src="item" alt>
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommonGallary",
+  props: {
+    imgs: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        // 修改轮播图底部显示样式
+        paginationType: "fraction",
+        // 监听父组件或组件dom元素发生变化，自动自我刷新
+        observeParents: true,
+        observer: true
+      }
+    };
+  },
+  methods: {
+    handleGallaryClick() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.container >>> .swiper-container {
+  overflow: inherit;
+}
+
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: #000;
+  z-index: 99;
+
+  .wrapper {
+    width: 100%;
+    padding-bottom: 100%;
+    height: 0;
+  }
+}
+
+.gallary-img {
+  width: 100%;
+}
+
+.swiper-pagination {
+  color: #fff;
+  bottom: -1rem;
+}
+</style>
