933230f0466f92825352d265a8890436faec1dc8
ida <ida@tansuyun.cn>
2019-05-06 20:43:01 +0800

    布局detail的header+利用原生js获取滚动高度

--- src/pages/detail/components/Header.vue
+++ src/pages/detail/components/Header.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <div class="header">
-      <div class="header-abs"></div>
-      <div class="header-fixed"></div>
+      <router-link tag='div' to='/' class="header-abs" v-show='showAbs'>
+        <i class="iconfont iconreturn"></i>
+      </router-link>
+      <div class='header-fixed' v-show='!showAbs'>
+        <router-link tag='div' to='/'>
+          <div><i class="iconfont iconreturn header-fixed-back"></i></div>
+        </router-link>
+      景点详情
+      </div>
     </div>
   </div>
 </template>
@@ -10,16 +17,59 @@
 <script>
 import CommonGallary from "common/gallary/Gallary";
 export default {
-  name: "DetailHeader"
+  name: "DetailHeader",
+  data() {
+    return {
+      showAbs: true
+    }
+  },
+  methods: {
+    handleScroll() {
+      const top = document.documentElement.scrollTop
+      if(top>60) {
+        this.showAbs = false;
+      } else {
+        this.showAbs = true;
+      }
+    }
+  },
+  activated() {
+    window.addEventListener('scroll',this.handleScroll)
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl';
 
-.header-abs {
+.header-abs 
   position: absolute;
   left: 0.2rem;
   top: 0.2rem;
-}
+  width: .8rem;
+  height : .8rem;
+  line-height: .8rem;
+  text-align: center;
+  border-radius: .4rem;
+  background: rgba(0,0,0,.8)
+.iconreturn 
+  color:#fff;
+  font-size: .4rem;
+.header-fixed 
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: $headerHeight;
+  line-height: $headerHeight;
+  text-align: center;
+  color: #fff;
+  background: $bgColor;
+  font-size: .32rem;
+.header-fixed-back 
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: .64rem;
+  text-align: align;
 </style>
