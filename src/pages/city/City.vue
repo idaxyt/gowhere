65d95f4494f2d7665f6621a603878be5ce0c9117
idaxyt <waitingida@live.com>
2019-05-06 11:03:39 +0800

    渲染城市数据+字母表渲染+兄弟组件间传值

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,53 +1,54 @@
 <template>
-    <div>
-        <city-header></city-header>
-        <city-search></city-search>  
-        <city-list  :cities='cities' :hot='hotCities'></city-list>
-        <alphabet :alpha='cities'></alphabet>
-    </div>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+    <city-list :cities="cities" :hot="hotCities" :alpha="alpha"></city-list>
+    <alphabet :alpha="cities" @change="handleClick"></alphabet>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import CityHeader from './components/Header'
-import CitySearch from './components/Search'
-import CityList from './components/List'
-import Alphabet from './components/Alphabet'
+import axios from "axios";
+import CityHeader from "./components/Header";
+import CitySearch from "./components/Search";
+import CityList from "./components/List";
+import Alphabet from "./components/Alphabet";
 export default {
-    name: 'City',
-    components: {
-        CityHeader,
-        CitySearch,
-        CityList,
-        Alphabet
+  name: "City",
+  components: {
+    CityHeader,
+    CitySearch,
+    CityList,
+    Alphabet
+  },
+  data() {
+    return {
+      cities: {},
+      hotCities: [],
+      alpha: ""
+    };
+  },
+  methods: {
+    getCityInfo() {
+      axios.get("/api/city.json").then(this.handleCityInfoSucc);
     },
-    data() {
-        return {
-            cities: {
-
-            },
-            hotCities: []
-        }
+    handleCityInfoSucc(res) {
+      res = res.data;
+      if (res.ret && res.data) {
+        const data = res.data;
+        this.cities = data.cities;
+        this.hotCities = data.hotCities;
+      }
     },
-    methods:{
-        getCityInfo() {
-            axios.get('/api/city.json').then(this.handleCityInfoSucc)
-        },
-        handleCityInfoSucc(res) {
-            res = res.data;
-            if(res.ret && res.data) {
-                const data = res.data
-                this.cities = data.cities
-                this.hotCities = data.hotCities
-            }
-        }
-    },
-    mounted() {
-        this.getCityInfo()
-    },
-}
+    handleClick(e) {
+      this.alpha = e;
+    }
+  },
+  mounted() {
+    this.getCityInfo();
+  }
+};
 </script>
 
 <style lang='stylus' scoped>
-
 </style>
--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,33 +1,42 @@
 <template>
-    <ul class="list">
-        <li class='item' v-for="(item,key) of alpha" :key="key">{{key}}</li>
-    </ul>
+  <ul class="list">
+    <li class="item" v-for="(item,key) of alpha" :key="key" @click="handleClick">{{key}}</li>
+  </ul>
 </template>
 
 <script>
 export default {
-    name: 'Alphabet',
-    props: {
-        alpha: Object
+  name: "Alphabet",
+  props: {
+    alpha: Object
+  },
+  methods: {
+    handleClick(e) {
+      this.$emit("change", e.target.innerText);
     }
-}
+  }
+};
 </script>
 
 <style lang='stylus' scoped>
-@import '~styles/varibles.styl'
-.list
-    // 垂直方向上居中
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    // 垂直方向上居中    
-    position: absolute;
-    top: 1.58rem;
-    right:0;
-    bottom:0;
-    width: .4rem;
-    .item
-        line-height: .4rem;
-        color: $bgColor;
-        text-align: center;
+@import '~styles/varibles.styl';
+
+.list {
+  // 垂直方向上居中
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  // 垂直方向上居中
+  position: absolute;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0;
+  width: 0.4rem;
+
+  .item {
+    line-height: 0.4rem;
+    color: $bgColor;
+    text-align: center;
+  }
+}
 </style>
