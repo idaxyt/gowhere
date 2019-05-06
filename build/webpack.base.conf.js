b0495af72bb367d5177500d13aab18b845823745
idaxyt <waitingida@live.com>
2019-05-06 18:51:36 +0800

    更改src的common文件路径

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -35,7 +35,8 @@ module.exports = {
         alias: {
             'vue$': 'vue/dist/vue.esm.js',
             '@': resolve('src'),
-            'styles': resolve('src/assets/styles')
+            'styles': resolve('src/assets/styles'),
+            'common': resolve('src/common')
         }
     },
     module: {
