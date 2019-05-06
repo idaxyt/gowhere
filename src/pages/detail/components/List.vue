42b7972b39f5f5c0fb3babe7202a68ab69d40978
ida <ida@tansuyun.cn>
2019-05-06 21:31:00 +0800

    布局detail-list组件

---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="item" v-for='(item,index) in list' :key='index'>
+      <div class="item-title border-bottom">
+        <i class="item-title-icon"></i>{{item.title}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DetailList",
+  data() {
+    return {
+      
+    }
+  },
+  props: {
+    list: Array
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+.item-title-icon
+  position: relative;
+  left: .06rem;
+  top: .08rem;
+  display: inline-block;
+  width: .36rem;
+  height: .36rem;
+  background: url(//s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+  margin-right: .1rem;
+  background-size: .4rem 3rem;
+.item-title 
+  line-height: .8rem;
+  font-size: .3rem;
+  padding: 0 .2rem;
+</style>
