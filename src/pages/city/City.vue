52ca5976722ea646cbfeb6cc575cd6b5186ddf31
idaxyt <waitingida@live.com>
2019-05-06 12:59:57 +0800

    城市搜索逻辑

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :cities="cities" :hot="hotCities" :alpha="alpha"></city-list>
     <alphabet :alpha="cities" @change="handleClick"></alphabet>
   </div>
