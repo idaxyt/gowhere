4cb0099acaa3f7b7eae265420422d268c41bb206
idaxyt <waitingida@live.com>
2019-05-06 19:06:30 +0800

    初始化detail-header+在detail中引入header

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="header-abs"></div>
+      <div class="header-fixed"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CommonGallary from "common/gallary/Gallary";
+export default {
+  name: "DetailHeader"
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.header-abs {
+  position: absolute;
+  left: 0.2rem;
+  top: 0.2rem;
+}
+</style>
